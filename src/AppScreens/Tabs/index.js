7d3ec45b597e8576a32e2b0312d1a615957d5775
Fernando <fer.fayet@usp.br>
2021-01-15 14:30:07 -0300

    five main tabs added

--- src/AppScreens/Tabs/index.js
+++ src/AppScreens/Tabs/index.js
@@ -1,29 +1,76 @@
-import React from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Questionnaire = () => {
 
-    return (
-        <View style={styles.container}>
-            <Text>
-                Tabs Screen
-            </Text>
-            <Text>
-                Here will be located the tabs of the app (map, profile, events, timeline...)   
-            </Text>
-        </View>
-    )
+// PLACE HOLDER FUNCTIONS
+// To be replaced by separet components
+function TimelineScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Timeline</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        width:Dimensions.get("window").width,
-        alignItems:"center",
-        marginTop: 200,
-        marginLeft: 30,
-        marginRight: 30,
-    },
-})
+function EventsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Events</Text>
+    </View>
+  );
+}
 
-export default Questionnaire
+function MapsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Map</Text>
+      </View>
+    );
+}
+
+function AppointmentsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Next appointments</Text>
+      </View>
+    );
+}
+
+function ProfileScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Profile</Text>
+      </View>
+    );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Timeline" component={TimelineScreen} />
+        <Tab.Screen name="Events" component={EventsScreen} />
+        <Tab.Screen name="Map" component={MapsScreen} />
+        <Tab.Screen name="Appointments" component={AppointmentsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex:1,
+//         width:Dimensions.get("window").width,
+//         alignItems:"center",
+//         marginTop: 200,
+//         marginLeft: 30,
+//         marginRight: 30,
+//     },
+// })
