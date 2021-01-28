2d18ca46acdda0904425c65372b2a6933bdc2487
Eduardo Pirro <edpirro.ep@gmail.com>
2021-01-27 21:18:28 -0300

    event join & list joined

--- back-end/controllers/EventController.js
+++ back-end/controllers/EventController.js
@@ -5,25 +5,70 @@ const db = require("../configs/firebase").database();
 module.exports =  {
 
     list(req, res) {
-        // req.user = {uid: "1"};
-        db.ref(`events/${req.user.uid}`).orderByKey().once("value", data => {
+        db.ref(`events`).orderByKey().once("value", data => {
             if(!data.val()) return res.status(200).send([]);
-            let ret = Object.values(data.val());
-            if(ret) ret = ret.reverse();
-            return res.status(200).send(ret);
+            const obj = data.val();
+            let _data = Object.values(obj);
+            let ids = Object.keys(obj);
+            if(_data) {
+                _data = _data.reverse();
+                ids = ids.reverse();
+            }
+            for(let i = 0; i < _data?.length; ++i) _data[i].id = ids[i];
+            return res.status(200).send(_data);
         })
     },
 
+    listCreated(req, res) {
+        // req.user = {uid: "1"};
+        db.ref(`events`).orderByChild("createdBy").equalTo(req.user.uid).once("value", data => {
+            if(!data.val()) return res.status(200).send([]);
+            const obj = data.val();
+            let _data = Object.values(obj);
+            let ids = Object.keys(obj);
+            if(_data) {
+                _data = _data.reverse();
+                ids = ids.reverse();
+            }
+            for(let i = 0; i < _data.length; ++i) _data[i].id = ids[i];
+            return res.status(200).send(_data);
+        });
+    },
+
+    listJoined(req, res) {
+        // req.user = {uid: "1"}; // testing
+        db.ref(`users/${req.user.uid}/joinedEvents`).orderByKey().once("value", joinedEvents => {
+            if(!joinedEvents.val()) return res.status(200).send([]);
+            let ret = Object.values(joinedEvents.val());
+            return res.status(200).send(ret);
+        });     
+    },
+
     create(req, res) {
-        // req.user = {uid: "1", email: "test"}; // testing
-        db.ref(`events/${req.user.uid}`).push({
-            createdBy: req.user.email,
-            sports: req.body.sports,
-            date: req.body.date
-        }, (e) => {
+        // req.user = {uid: "1"}; // testing
+        const fields = ["name", "description", "address", "maxCap", "price", "sport"]
+        const miss = [];
+        const evnt = {};
+        for(let f of fields) {
+            if(!req.body[f]) miss.push(f);
+            else evnt[f] = req.body[f];
+        }
+        if(miss.length) return res.status(400).send(`Missing field(s): ${miss}`);
+
+        evnt.createdBy = req.user.uid
+
+        db.ref(`events`).push(evnt, (e) => {
             if(e) return res.status(400).send(e);
             return res.sendStatus(201);
         });
+    },
+
+    join(req, res) {
+        // req.user = { uid: "1" }; // testing
+        const { event } = req.body;
+        db.ref(`events/${event.id}/participants`).push(req.user);
+        db.ref(`users/${req.user.uid}/joinedEvents`).push(event);
+        return res.sendStatus(200);
     }
 
 }
